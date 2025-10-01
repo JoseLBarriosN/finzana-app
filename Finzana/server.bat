9821277666660396b9c124ddaec8963e8a842ab9
JoseLBarriosN <barrios.nunez.joseluis@gmail.com>
2025-10-01 00:21:52 -0600

    Delete Finzana directory

--- Finzana/server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Cambia al directorio deseado
-cd C:\Users\licht\OneDrive\Escritorio\Finzana
-rem Inicia el servidor de Python
-python3 -m http.server 3000
-rem Pausa el script. La ventana no se cerrará hasta que presiones una tecla.
-pause
-
-
