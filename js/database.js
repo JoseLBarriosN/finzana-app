8a94fec1bd89377ab46a5869ff703da5a5e003db
JoseLBarriosN <barrios.nunez.joseluis@gmail.com>
2025-11-13 22:58:22 -0600

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1719,8 +1719,7 @@ const database = {
             console.error("Error agregando comisión:", error);
             return { success: false, message: `Error: ${error.message}` };
         }
-    }
-};
+    };
 
 // EN database.js - AÑADE ESTAS FUNCIONES DENTRO DEL OBJETO 'database = {'
 
@@ -1867,6 +1866,7 @@ const database = {
             return { success: false, message: `Error: ${error.message}` };
         }
     },
+
 
 
 
